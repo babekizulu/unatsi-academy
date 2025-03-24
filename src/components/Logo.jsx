a418ea066736b7fb9a0d5473011ae0483571a499
babekizulu <74798044+babekizulu@users.noreply.github.com>
2025-03-24 17:21:16 +0200

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 //components
 import Link from "./Link";
 //icons
-import logo from './icons/UNATSI_ACADEMY_59X59.svg';
+import logo from './icons/UNATSI_ACADEMY_59x59.svg';
 
 function Logo() {
     return (
